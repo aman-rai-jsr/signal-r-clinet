d510d911004856e3ad31dde570cdb30713ddf680
Aman Rai <aman.rai@ggktech.com>
2020-08-26 13:56:19 +0530

    added on event for demonsatration

--- src/component/SignalRComponent.js
+++ src/component/SignalRComponent.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import { HubConnection } from 'signalr-client-react';
+import React from "react";
+import { HubConnection } from "signalr-client-react";
 
-function SignalRComponent()
-{
-    // Hub Url
-    const hubUrl = '/test';
+function SignalRComponent() {
+  let [messages, setMessages] = React.useState(["Some dummy message"]);
 
-    let connection = new HubConnection(hubUrl);
+  // Hub Url
+  const hubUrl = "/test";
 
-    // add second parameter if any configurtions needs to be added except the Hub URL as show below
-    // let connection = new HubConnection(hubUrl,{});
+  let connection = new HubConnection(hubUrl);
 
-    // to intially connect with hub when componet renders
-    React.useEffect(()=>{
-        start();
+  // add second parameter if any configurtions needs to be added except the Hub URL as show below
+  // let connection = new HubConnection(hubUrl,{});
+
+  // to intially connect with hub when componet renders
+  React.useEffect(() => {
+    start();
+  });
+
+  // function which holds the code for creating a connection
+  async function start() {
+    try {
+      await connection.start();
+      console.log("connected");
+    } catch (err) {
+      console.log(err);
+      // Try to reconnect if fails
+      setTimeout(() => start(), 5000);
+    }
+  }
+
+  // Reconnect when connection drop
+  connection.onclose(async () => {
+    await start();
+  });
+
+  // Update the event listner name as per the Hub
+  connection.on("newMessage", (data) => {
+    console.log(data);
+    let temp = messages;
+    temp.push(data);
+    setMessages([...temp]);
+  });
+
+  const renderMessages = () => {
+    return messages.map((message) => {
+      return <div>{JSON.stringify(message)}</div>;
     });
+  };
 
-    // function which holds the code for creating a connection
-    async function start() {
-        try {
-            await connection.start();
-            console.log("connected");
-        } catch (err) {
-            console.log(err);
-            // Try to reconnect if fails
-            setTimeout(() => start(), 5000);
-        }
-    };
-
-    // Reconnect when connection drop
-    connection.onclose(async () => {
-        await start();
-    });
-
-    return <h1>Hello it works</h1>
+  return (
+    <>
+      <h1>Hello it works</h1>
+      <div>{renderMessages()}</div>
+    </>
+  );
 }
 
 export default SignalRComponent;
